d37ab8898b4faf0eb2b31d161199d123bf75f453
7799 <hayashi7799@gmail.com>
2014-05-26 10:41:28 +0900

    modified:   AndroidManifest.xml 	modified:   bin/AndroidManifest.xml 	modified:   src/example/android/contextmenusample/ContextMenuSampleActivity.java

--- src/example/android/contextmenusample/ContextMenuSampleActivity.java
+++ src/example/android/contextmenusample/ContextMenuSampleActivity.java
@@ -1,15 +1,95 @@
 package example.android.contextmenusample;
 
-import android.os.Bundle;
+import java.util.ArrayList;
+import java.util.List;
+
 import android.app.Activity;
+import android.os.Bundle;
+import android.util.SparseBooleanArray;
+import android.view.ActionMode;
 import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.widget.AbsListView.MultiChoiceModeListener;
+import android.widget.ArrayAdapter;
+import android.widget.ListAdapter;
+import android.widget.ListView;
+import android.widget.TextView;
+
+
+
 
 public class ContextMenuSampleActivity extends Activity {
+
+	List<String> list;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_context_menu_sample);
+
+		list = new ArrayList<String>();
+		list.add("選択1");
+		list.add("選択2");
+		list.add("選択3");
+
+		ListAdapter adapter = new ArrayAdapter<String>(this,
+				android.R.layout.simple_list_item_checked,
+				list);
+
+		ListView listView = (ListView) findViewById(R.id.lv_item);
+		listView.setAdapter(adapter);
+
+		listView.setChoiceMode(ListView.CHOICE_MODE_MULTIPLE_MODAL);
+
+		listView.setMultiChoiceModeListener(new ListViewChoiceListener());
+	}
+
+	class ListViewChoiceListener implements MultiChoiceModeListener {
+
+
+		public boolean onActionItemClicked(ActionMode mode, MenuItem item) {
+			// TODO 自動生成されたメソッド・スタブ
+			ListView listView = (ListView) findViewById(R.id.lv_item);
+			TextView textView = (TextView) findViewById(R.id.tv_message);
+
+			SparseBooleanArray list = listView.getCheckedItemPositions();
+			String selectedItem = "(選択されたアイテム:";
+			for (int i = 0; i < list.size(); i++) {
+				selectedItem += " " + listView.getItemAtPosition(list.keyAt(i));
+			}
+			selectedItem += ")";
+
+
+			textView.setText("コンテキストメニューで選択:" + item.getTitle() + selectedItem);
+			mode.finish();
+
+			return true;
+		}
+
+		public boolean onCreateActionMode(ActionMode mode, Menu menu) {
+			// TODO 自動生成されたメソッド・スタブ
+			MenuInflater inflater = mode.getMenuInflater();
+			inflater.inflate(R.menu.context_menu_sample, menu);
+
+			return true;
+		}
+
+		public void onDestroyActionMode(ActionMode mode) {
+			// TODO 自動生成されたメソッド・スタブ
+
+		}
+
+		public boolean onPrepareActionMode(ActionMode mode, Menu menu) {
+			// TODO 自動生成されたメソッド・スタブ
+			return false;
+		}
+
+		public void onItemCheckedStateChanged(ActionMode mode, int menu,
+				long arg2, boolean arg3) {
+			// TODO 自動生成されたメソッド・スタブ
+
+		}
 	}
 
 	@Override
